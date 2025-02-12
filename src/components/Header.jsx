210c40a1ff1b19625f88ca84d4e9241adb441d22
francisco.bruno@armateus.com.br <bruno-amil@hotmail.com>
2025-02-11 22:44:11 -0300

    style: define style int text

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import logoSpotify from "../assets/logo/spotify-logo.png";
 
 export const Header = () => {
   return (
-    <div>
+    <div className="header">
       <img src={logoSpotify} alt="Logo Spotify" />
       <a href="/">
         <h1>Spotify</h1>
