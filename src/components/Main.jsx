026af6d54ff64e8ca68968d0812d64b4980619db
francisco.bruno@armateus.com.br <bruno-amil@hotmail.com>
2025-02-12 22:18:19 -0300

    feat: quantity numbers

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -6,7 +6,7 @@ import { songsArray } from "../assets/database/songs";
 const Main = () => {
   return (
     <div className="main">
-      <ItemList title="Artistas" items={5} itemsArray={artistArray} path="/artists" idPath="/artist" />
+      <ItemList title="Artistas" items={10} itemsArray={artistArray} path="/artists" idPath="/artist" />
       <ItemList title="Musicas" items={20} itemsArray={songsArray} path="/songs" idPath="/songs" />
     </div>
   );
