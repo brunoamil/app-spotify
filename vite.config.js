3b3636cb0018bc30453547e3596ce14b60692f2a
francisco.bruno@armateus.com.br <bruno-amil@hotmail.com>
2025-02-16 13:22:17 -0300

    fix: change port

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
     target: "esnext",
-  }
-})
+  },
+  server: {
+    host: "0.0.0.0", // Aqui está o segredo
+    port: 5173, // Render usa a variável PORT
+  },
+});
